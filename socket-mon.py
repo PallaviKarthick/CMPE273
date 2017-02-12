bc3225bcea8a16eba686db4749325b80458f1b2b
pallavikarthick <pallaviatg@gmail.com>
2017-02-12 14:38:29 -0800

    psutil v2

--- socket-mon.py
+++ socket-mon.py
@@ -2,24 +2,27 @@ import collections
 import psutil
 from itertools import groupby
 
+#sconn object definition
 sconn = collections.namedtuple('sconn', ['fd', 'family', 'type',
 'laddr','raddr','status','pid'])
 
-sconn_list1 = psutil.net_connections()
-# Access the namedtuple as a dict
+
+#list of socket connections as a namedtuple
+ls_sconn = psutil.net_connections()
 
 
 
+# processes that has valid socket connections
 ls_valid_sconn=[]
-for item in sconn_list1:
-    if len(item.__dict__['raddr'])!=0:
+for item in ls_sconn:
+    if len(item.__dict__['raddr'])!=0 and len(item.__dict__['laddr'])!=0:
         ls_valid_sconn.append(item)
 print "Total Number of processes that has valid socket connections " , len(ls_valid_sconn) ,"\n"
 
 
+
+#Group by the PID and sort by number of the connections per process in descending order
 pid_Groups = groupby(ls_valid_sconn, lambda a: (a.pid))
-
-
 ls = {}
 for pid, group in pid_Groups:
     length = len(list(group))
@@ -31,6 +34,10 @@ for pid , count in ls_No_Of_Conn:
     print "PID: %s - Total number of Connections: %s" % (pid, count )
 
 print "\n"
+
+
+
+# print PID, Local Address , Remote Address and Status of connection
 templ = "%-8s %-20s %-25s %-10s \n"
 print(templ % ("pid", "laddr", "raddr", "status"))
 #print  """"pid","laddr","raddr","status" """
